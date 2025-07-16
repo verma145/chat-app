321545a803454510d85e6ca63fd98074fd6c0e56
verma <verma1454u@email.com>
2025-07-16 11:40:57 +0530

    fix: use process.env.PORT for rinder deployment

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
